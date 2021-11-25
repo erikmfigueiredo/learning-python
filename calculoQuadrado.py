2b47809e8a7cf4f239848c65eaafd62a11fe9777
Erik Figueiredo <95001589+erikmfigueiredo@users.noreply.github.com>
2021-11-25 18:36:01 -0300

    Create calculoQuadrado.py

---
+++ calculoQuadrado.py
@@ -0,0 +1,10 @@
+# CÁCULO DA ÁREA E PERIMETRO DO QUADRADO
+
+lado = input("Digite o valor correspondente ao lado de um quadrado: ")
+
+ladoint = int(lado)
+
+area = ladoint * ladoint
+perimetro = ladoint * 4
+
+print("perímetro:",perimetro,"- área:",area)
