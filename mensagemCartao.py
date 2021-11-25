af7d317ac6ef6bd40940b18506897c5d42852a01
Erik Figueiredo <95001589+erikmfigueiredo@users.noreply.github.com>
2021-11-25 18:36:54 -0300

    Create mensagemCartao.py

---
+++ mensagemCartao.py
@@ -0,0 +1,9 @@
+# MENSAGEM DE FATURA DO CARTÃO
+
+nome = input("Digite o nome do cliente: ")
+vencimentoDia = input("Digite o dia de vencimento: ")
+vencimentoMes = input("Digite o mês de vencimento: ")
+valor = input("Digite o valor da fatura: ")
+
+print("Olá,",nome)
+print("A sua fatura com vencimento em",vencimentoDia,"de",vencimentoMes,"no valor de R$",valor,"está fechada.")
