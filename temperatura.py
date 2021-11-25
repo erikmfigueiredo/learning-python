00acd52a175e4402306763daaad8e685372979c5
Erik Figueiredo <95001589+erikmfigueiredo@users.noreply.github.com>
2021-11-25 18:28:57 -0300

    Create temperatura.py

---
+++ temperatura.py
@@ -0,0 +1,7 @@
+# CONVERSOR DE TEMPERATURA FAHRENHEIT PARA CELSIUS
+
+temperaturaFahrenheit = input("Entre com a temperatura em Fahrenheit: ")
+
+temperaturaCelsius = (float(temperaturaFahrenheit) - 32) * 5 / 9
+
+print("A temperatura em Celsius é: ",temperaturaCelsius)
