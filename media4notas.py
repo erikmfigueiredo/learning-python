d605627a2e127ab6792d132109e0769b52e2fff5
Erik Figueiredo <95001589+erikmfigueiredo@users.noreply.github.com>
2021-11-25 18:36:24 -0300

    Create media4notas.py

---
+++ media4notas.py
@@ -0,0 +1,15 @@
+# MÉDIA ARITMÉDICA DE 4 NOTAS
+
+nota1 = input("Digite a primeira nota:")
+nota2 = input("Digite a segunda nota:")
+nota3 = input("Digite a terceira nota:")
+nota4 = input("Digite a quarta nota:")
+
+nota1int = int(nota1)
+nota2int = int(nota2)
+nota3int = int(nota3)
+nota4int = int(nota4)
+
+media=(nota1int+nota2int+nota3int+nota4int)/4
+
+print("A média aritmédica é",media)
