96bf732c7e4128fc21af5bacf15ec78e663eee68
Erik Figueiredo <95001589+erikmfigueiredo@users.noreply.github.com>
2021-11-29 19:23:45 -0300

    Create ordenacao.py

---
+++ ordenacao.py
@@ -0,0 +1,11 @@
+# VERIFICANDO ORDENAÇÃO
+
+a = int(input("Entre com A: "))
+b = int(input("Entre com B: "))
+c = int(input("Entre com C: "))
+
+if a<b and b<c:
+  print("crescente")
+
+else:
+  print("não está em ordem crescente")
