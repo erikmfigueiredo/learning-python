507d77149d87ca17224e8cc03e3a6667151b4809
Erik Figueiredo <95001589+erikmfigueiredo@users.noreply.github.com>
2022-03-04 18:21:01 -0300

    Add files via upload

--- fatorial.py
+++ fatorial.py
@@ -1,8 +1,9 @@
-n = int(input("Digite o valor de n: "))
-valor = 1
-
-while n>1:
-  valor = valor * n
-  n = n-1
-
-print(valor)
+n = int(input("Digite um número inteiro: "))
+while n >= 0:
+    fatorial=1
+    while n > 1:
+        fatorial = fatorial * n
+        n = n - 1
+    print (fatorial)
+    n = int(input("Digite um número inteiro: "))
+     
---
+++ previsaoVendas.py
@@ -0,0 +1,7 @@
+c = 200 # valor da constante
+
+mes = int(input("Digite o mês que deseja saber o resultado: "))
+
+r = c * mes 
+
+print(f"A quantidade de peças para o mês {mes} será {r}")
